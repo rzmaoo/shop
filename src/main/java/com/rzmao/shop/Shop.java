409e1efcb8d06472dee1a4f31d53f4015ec0dca5
rzmao <rzmaoo@outlook.com>
2026-06-28 02:28:01 +0800

    init

---
+++ src/main/java/com/rzmao/shop/Shop.java
@@ -0,0 +1,11 @@
+package com.rzmao.shop;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Shop.MOD_ID)
+public final class Shop {
+    public static final String MOD_ID = "shop";
+
+    public Shop() {
+    }
+}
